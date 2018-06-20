e47ebb0351d4c289583c9ed5817ae2876a4ac401
kscardina <popmedic@gmail.com>
2018-06-20 10:19:20 -0600

    fix test coverage generation

--- test.sh
+++ test.sh
@@ -7,12 +7,23 @@
 #  Copyright © 2018 Kevin Scardina. All rights reserved.
 
 test(){
-    xcodebuild \
-        -project "${PROJ}" \
-        -scheme "${SCHEME}" \
-        -sdk iphonesimulator \
-        -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.2' \
-        -derivedDataPath "${BUILDDIR}" \
-        -enableCodeCoverage YES \
-        test
+    if [ -z ${1+x} ] 
+    then
+        xcodebuild \
+            -project "${PROJ}" \
+            -scheme "${SCHEME}" \
+            -sdk iphonesimulator \
+            -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.2' \
+            -derivedDataPath "${BUILDDIR}" \
+            -enableCodeCoverage YES \
+            test
+    else
+        xcodebuild \
+            -project "${PROJ}" \
+            -scheme "${SCHEME}" \
+            -sdk iphonesimulator \
+            -destination 'platform=iOS Simulator,name=iPhone 8,OS=11.2' \
+            -enableCodeCoverage YES \
+            test
+    fi
 }
